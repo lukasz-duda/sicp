ecedd63b603d746bce91433b013d45b3ceb25175
Łukasz Duda <lduda@pivotal.pl>
2024-06-23 20:08:19 +0000

    Calculate square root

--- src/square.clj
+++ src/square.clj
@@ -1,3 +1,18 @@
 (ns square)
 
 (defn square [x] (* x x))
+
+(defn good-enough? [guess x]
+  (< (abs (- (square guess) x)) 0.001))
+
+(defn average [a b] (/ (+ a b) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn sqrt-iter [guess x]
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(defn sqrt [x] (sqrt-iter 1.0 x))
--- src/square_test.clj
+++ src/square_test.clj
@@ -1,8 +1,16 @@
 (ns square-test
   (:require [clojure.test :refer [deftest is testing]]
-            [square :refer [square]]))
+            [square :refer [sqrt square]]))
 
 (deftest square-test
   (testing "integers"
     (is (= 4 (square 2)))
     (is (= 9 (square 3)))))
+
+(defn close? [x y]
+  (< (abs (double (- x y))) 0.001))
+
+(deftest sqrt-test
+  (testing "integers"
+    (is (close? 2 (sqrt 4)))
+    (is (close? 3 (sqrt 9)))))
